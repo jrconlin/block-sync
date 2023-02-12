d8371f39217163801cfeb39315d5492afd09b52e
jrconlin <me+crypt@jrconlin.com>
2023-02-11 16:11:30 -0800

    Version 1.0.0
    * Mastodon 4.1 compatible.
    * Removed `--dump` & `--load`
    * `app_key` and `home` are now optional

--- README.md
+++ README.md
@@ -9,16 +9,25 @@ pull the block lists from a set of trusted servers and then add those sites to m
 
 ## How To
 
-This is a work in progress, possibly zeta level code, so, you know. 
+This is a work in progress, possibly zeta level code, so, you know.
 
 You can specify options using the CLI, Environment, or a static file in `settings.config`.
 
-It will grab your block list, then grab the public block lists using the HTTP API. It then adds any hosts that are not included in your local block list using your local server API. (see Setup below)
+This will grab the public block lists using the HTTP API for the sites listed in `--remote`. It then generates a Mastodon compatible block file
+(default: `domain_blocks.csv`). If you specify the `--home {HOST}`, it will attempt to import the blocks directly into your server.
 
-This uses python, so simple python setup rules apply. 
+For example:
+
+```bash
+python block_sync.py --remote hachyderm.io --remote infosec.exchange --output my_blocklist.csv
+```
+
+will fetch block lists from https://hacyderm.io, and https://infosec.exchange, and write out the bocks to `my_blocklist.csv`
+
+This uses python, so simple python setup rules apply.
 e.g.
 
-```bash 
+```bash
 $ python3 -mvenv venv
 $ exec venv/bin/activate
 (venv)$ pip install -r requirements.txt
@@ -26,7 +35,9 @@ $ exec venv/bin/activate
 
 ## Setup
 
-Surprisingly, while there's an HTTP API to add sites, the permission to add them is not readily available to applications. You will need to access the database and manually set the token's permission.
+This version will output a `domain_blocks.csv` file that you can import into your instance. A sample `settings.conf.sample` is provided. If you wish to use it, please rename it to `settings.conf`
+
+The older interface still exists, and can write the domains into your instance, but some extra steps are still requrired. Surprisingly, while there's an HTTP API to add sites, the permission to add them is not readily available to applications. You will need to access the database and manually set the token's permission.
 
 ```postgres
 UPDATE oauth_access_tokens
--- block-sync.py
+++ block-sync.py
@@ -2,12 +2,14 @@
 """
 # Block Sync
 
-Read a collection of remote sites block list and add them to
-your own instance.
+Read a collection of remote sites block list and dump out a
+mastodon 4.1 compatible `domain_blocks.csv` file. Optionally
+this will also add them to your own instance.
 
 ## Notes:
 
-You'll need to add a permission to the owner of the API_KEY.
+If you want to use the --update_home option, you'll need to
+add a permission to the owner of the API_KEY.
 In the database:
 
 ```postgres
@@ -18,25 +20,43 @@ UPDATE oauth_access_tokens
 
 """
 
+from io import FileIO
 import logging
 import json
+import re
 from urllib.parse import urlencode
 
 import requests
 import configargparse
 
 
+VERSION = "1.0.0"
+
+
 def config():
     """read in the configuration."""
-    parser = configargparse.ArgParser(default_config_files=["settings.conf"])
+    parser = configargparse.ArgParser(
+        default_config_files=["settings.conf"],
+        description=f"""Read a collection of remote sites block list and dump out a
+mastodon 4.1 compatible `domain_blocks.csv` file. Optionally
+this will also add them to your own instance. Version {VERSION}"""
+        )
     parser.add("-c", "--config", is_config_file=True, help="config file path")
     parser.add("-v", "--verbose", action="store_true", help="verbose mode")
     parser.add("--app_key", help="your server's access key", env_var="APP_KEY")
     parser.add("--dry_run", action="store_true", help="just fetch and compare")
-    parser.add("--dump", help="dump the collected list of sites to specified file")
-    parser.add("--home", required=True, help="your instance url", env_var="HOME_HOST")
-    parser.add("--load", help="load the collected list of sites from specified file")
-    parser.add("--log_level", default="error", help="logging level [debug,info,warn,error]")
+    parser.add(
+        "--output",
+        default="domain_blocks.csv",
+        env_var="OUTPUT",
+        help="dump the collected list of sites to specified file",
+    )
+    parser.add("--home", help="your instance url", env_var="HOME_HOST")
+    parser.add(        "--log_level",
+        default="error",
+        env_var="LOGGING",
+        help="logging level [debug,info,warn,error]",
+    )
     parser.add(
         "--remote",
         nargs="+",
@@ -48,22 +68,21 @@ def config():
     return settings
 
 
-def merge(old: dict[str, dict], data: list[dict]) -> dict[str, dict]:
+def merge(old: dict[str, dict], data: list[dict], origin: str) -> dict[str, dict]:
     """merge the new data set into the collection we've got so far"""
     for item in data:
         if not item.get("domain") or not item.get("severity"):
             raise Exception("data missing required elements")
         if "*" in item.get("domain"):
             continue
-        old[item.get("domain")] = {
-            "severity": item.get("severity"),
-            "comment": item.get("comment"),
-        }
+        if origin:
+            item["private_comment"] = f"from: {origin}"
+        old[item.get("domain")] = item
     return old
 
 
 def fetch(sites: list[str]) -> dict[str, dict]:
-    """fetch remote blocl list"""
+    """fetch remote block list using the Public API"""
     result = {}
     block_list_template = "https://{site}/api/v1/instance/domain_blocks"
     for site in sites:
@@ -72,12 +91,12 @@ def fetch(sites: list[str]) -> dict[str, dict]:
         logging.debug(url)
         response = requests.get(block_list_template.format(site=site))
         if response.status_code != 200:
-            logging.error("{site} not publishing block list".format(site=site))
+            logging.error("🚨{site} not publishing block list".format(site=site))
             continue
         try:
-            result = merge(result, response.json())
+            result = merge(result, response.json(), site)
         except Exception as ex:
-            logging.error("Exception: {ex}".format(ex=ex))
+            logging.warning("⚠ Merge Exception: {ex}, continuing...".format(ex=ex))
             continue
     logging.debug("{}: {} sites".format(site, len(result)))
     return result
@@ -89,6 +108,7 @@ def compare(mine: dict[str, dict], theirs: dict[str, dict]) -> dict[str, dict]:
     Compare both keys against each other because domains can
     be shorter than we expect (e.g. foo.example.com vs example.com)
     """
+    logging.info("Generating differences")
     missing = {}
     for key in theirs.keys():
         use = True
@@ -106,13 +126,16 @@ def compare(mine: dict[str, dict], theirs: dict[str, dict]) -> dict[str, dict]:
 
 def apply_diff(home: str, auth: str, diff: dict[str, dict]):
     """Add the domains we've not seen yet to our instance."""
+    logging.info("Applying differences to home site")
     url = f"https://{home}/api/v1/admin/domain_blocks"
     creds = f"Bearer {auth}"
     for key in diff.keys():
+        data = diff.get("key")
         args = {
             "domain": key,
-            "severity": diff.get(key).get("severity"),
-            "comment": diff.get(key).get("comment"),
+            "severity": data.get("severity"),
+            "comment": data.get("comment"),
+            "private_comment": data.get("private_comment"),
         }
         body = urlencode(args)
         logging.debug(body)
@@ -124,6 +147,8 @@ def apply_diff(home: str, auth: str, diff: dict[str, dict]):
             raise Exception(
                 "Error updating blocks: {}:{}".format(resp.status_code, resp.text)
             )
+    logging.info("Changes applied.")
+
 
 def get_log_level(args) -> int:
     """Since match isn't availabe until 3.10"""
@@ -137,28 +162,53 @@ def get_log_level(args) -> int:
     return logging.ERROR
 
 
+def dump_csv(f: FileIO, sites_data: dict[str, dict]):
+    """Dump the list as a Mastodon 4.1 compatible CSV.
+
+    The `private_comment` may not import, but will show the origin of the block.
+    """
+    f.write(
+        "#domain,#severity,#reject_media,#reject_reports,#public_comment,#obfuscate\n"
+    )
+    for site, data in sorted(sites_data.items(), key=lambda t: t[0]):
+        severity = data.get("severity") or "silence"
+        row = [
+            site,
+            data.get("severity") or "silence",
+            data.get("reject_media") or "{}".format(severity == "suspend").lower(),
+            data.get("reject_report") or "{}".format(severity == "suspend").lower(),
+            '"{}"'.format(re.sub("([\"'])", r"\\\1", data.get("comment") or "")),
+            "false",
+        ]
+        f.write(",".join(row) + "\n")
+
+
 def main():
     args = config()
     level = get_log_level(args)
     logging.basicConfig(encoding="utf-8", level=level)
-    my_data = fetch([args.home])
-    # TODO: cache data to files
-    if args.load:
-        with open(args.load, "r") as f:
-            sites_data = json.loads(f.read())
-    else:
-        sites_data = fetch(args.remote)
-    if args.dump:
-        with open(args.dump, "w") as f:
-            f.write(json.dumps(sites_data))
-        return
-    # save_cache("sites.json", sites_data)
-    diff = compare(my_data, sites_data)
-    # save_cache("diffs.json", diff)
-    if not args.dry_run:
-        apply_diff(args.home, args.app_key, diff)
-    else:
+    my_data = {}
+    logging.info("Collecting public block lists")
+    sites_data = fetch(args.remote)
+
+    if args.output:
+        logging.info(f"Outputing collected blocks to {args.output}")
+        with open(args.output, "w") as f:
+            dump_csv(f, sites_data)
+    if args.home:
+        my_data = fetch([args.home])
+        diff = compare(my_data, sites_data)
+        if not args.dry_run:
+            if not args.app_key:
+                raise Exception(
+                    "Missing `app_key` argument. Can't update home instance."
+                )
+            apply_diff(args.home, args.app_key, diff)
+    if args.dry_run:
         print(json.dumps(diff, indent=2))
 
 
-main()
+try:
+    main()
+except Exception as e:
+    logging.error(e)
